07e9c83b5afe0c6ab2c7cb039272968d88876722
Ayush1M <worklife2442@gmail.com>
2023-02-09 23:25:11 +0530

    added tweets data.

---
+++ data.js
@@ -0,0 +1,57 @@
+const tweetsData = [   
+    {
+        handle: ``,
+        profilePic: ``,
+        likes: 27,
+        retweets: 10,
+        tweetText: ``,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },     
+    {
+        handle: ``,
+        profilePic: ``,
+        likes: 6500,
+        retweets: 234,
+        tweetText: ``,
+        replies: [
+                  {
+                handle: ``,
+                profilePic: ``,
+                tweetText: ``,
+            },
+                  {
+                handle: ``,
+                profilePic: ``,
+                tweetText: ``,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: ``,
+        profilePic: ``,
+        likes: 10,
+        retweets: 3,
+        tweetText: ``,
+        replies: [
+            {
+                handle: ``,
+                profilePic: ``,
+                tweetText: ``,
+            },
+            {
+                handle: ``,
+                profilePic: ``,
+                tweetText: ``,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
