a227851b516e9a563605767105fd97f7a61af254
Ayush1M <worklife2442@gmail.com>
2023-03-14 23:33:13 +0530

    added data attributes and feature to like or unlike a tweet.

--- script.js
+++ script.js
@@ -13,15 +13,15 @@ function getFeedHtml(){
             <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
-                <i class="fa-regular fa-comment-dots"></i>
+                <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-heart"></i>
+                <i class="fa-solid fa-heart" data-like="${tweet.uuid}"></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-retweet"></i>
+                <i class="fa-solid fa-retweet" data-retweet = "${tweet.uuid}"></i>
                     ${tweet.retweets}
                 </span>
             </div>   
@@ -36,5 +36,25 @@ function getFeedHtml(){
 function render(){
     document.getElementById('feed').innerHTML = getFeedHtml()
 }
-
 render()
+
+document.addEventListener("click", (e)=>{
+
+    if(e.target.dataset.like){
+        handleClick(e.target.dataset.like);
+    }
+    
+})
+
+function handleClick(tweetId){
+   const targetTweetObj = tweetsData.filter((tweet) => tweet.uuid === tweetId)[0]
+   
+   if(targetTweetObj.isLiked){
+    targetTweetObj.likes--
+    targetTweetObj.isLiked = false
+   }else{
+    targetTweetObj.likes++
+    targetTweetObj.isLiked = true
+   }
+   render()
+}
