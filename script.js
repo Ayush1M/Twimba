b442c91226309f20005ac2bbb4f1b4021b17f0b5
Ayush1M <worklife2442@gmail.com>
2023-03-17 23:49:05 +0530

    Added functionality to reply to a tweet.

--- script.js
+++ script.js
@@ -1,107 +1,132 @@
-import {tweetsData} from './data.js'
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
-
-const tweetInput = document.getElementById('tweet-area')
-
+import { tweetsData } from "./data.js";
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 // click events for the tweet handle
 
-document.addEventListener("click", (e)=>{
-    if(e.target.dataset.like){
-        handleClick(e.target.dataset.like);
-    } 
-    else if(e.target.dataset.retweet){
-        handleRetweet(e.target.dataset.retweet)
-    }
-    else if(e.target.dataset.reply){
-        handleReplyClick(e.target.dataset.reply)
-    }
-    else if(e.target.id === 'tweet-btn'){
-        handleTweetBtnClick()
-    }
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.like) {
+    handleClick(e.target.dataset.like)
+  } 
+  else if (e.target.dataset.retweet) {
+    handleRetweet(e.target.dataset.retweet)
+  } 
+  else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply)
+  } 
+  else if (e.target.id === "tweet-btn") {
+    handleTweetBtnClick();
+  } 
+  else if (e.target.id === "reply-tweet-btn") {
+    handleReplyBtnClick(e.target.dataset.replytext)
+  }
 })
 
 // handleClick function to control the like tweet icon
 
-function handleClick(tweetId){
-    const targetTweetObj = tweetsData.filter((tweet) => tweet.uuid === tweetId)[0]
-    
-    if(targetTweetObj.isLiked){
-     targetTweetObj.likes--
-     targetTweetObj.isLiked = false
-    }else{
-     targetTweetObj.likes++
-     targetTweetObj.isLiked = true
-    }
-    render()
+function handleClick(tweetId) {
+  const targetTweetObj = tweetsData.filter(
+    (tweet) => tweet.uuid === tweetId
+  )[0]
+
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--
+    targetTweetObj.isLiked = false
+  } else {
+    targetTweetObj.likes++
+    targetTweetObj.isLiked = true
+  }
+  render()
 }
 
 // handleRetweet function to control the retweet function
 
-function handleRetweet(tweetId){
-    const targetTweetObj = tweetsData.filter((tweet) => tweet.uuid === tweetId)[0]
+function handleRetweet(tweetId) {
+  const targetTweetObj = tweetsData.filter(
+    (tweet) => tweet.uuid === tweetId
+  )[0]
 
-    if(targetTweetObj.isRetweeted){
-        targetTweetObj.retweets--
-        targetTweetObj.isRetweeted = false
-    }else{
-        targetTweetObj.retweets++
-        targetTweetObj.isRetweeted = true
-    }
-    render()
+  if (targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets--
+    targetTweetObj.isRetweeted = false
+  } else {
+    targetTweetObj.retweets++
+    targetTweetObj.isRetweeted = true
+  }
+  render();
 }
 
 // toggle replies to hide/unhide
 
-function handleReplyClick(replyId){
-    document.getElementById(`replies-${replyId}`).classList.toggle('hidden')
+function handleReplyClick(replyId) {
+  document.getElementById(`replies-${replyId}`).classList.toggle("hidden")
 }
 
 // add a tweet
 
-function handleTweetBtnClick(){
-    if(tweetInput.value){
-        tweetsData.unshift({
-            handle: `@twimbabot000000`,
-            profilePic: `images/logo.png`,
-            likes: 0,
-            retweets: 0,
-            tweetText: tweetInput.value,
-            replies: [],
-            isLiked: false,
-            isRetweeted: false,
-            uuid: uuidv4()
-        })
-        render()
-        tweetInput.value = ''
-    }  
+function handleTweetBtnClick() {
+  const tweetInput = document.getElementById("tweet-area")
+
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@twimbabot000000`,
+      profilePic: `images/logo.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    })
+    render()
+    tweetInput.value = ""
+  }
+}
+
+// add a reply
+
+function handleReplyBtnClick(tweetId) {
+  const replyTweet = document.getElementById(`reply-tweet-${tweetId}`)
+
+  const targetTweetObj = tweetsData.filter((tweet) => tweet.uuid === tweetId)[0]
+
+  if (replyTweet.value.length >= 1) {
+    targetTweetObj.replies.push({
+      handle: `@Trollbot00000`,
+      profilePic: `images/logo.png`,
+      tweetText: replyTweet.value,
+      uuid: uuidv4()
+    })
+    render()
+    console.log(targetTweetObj)
+  }
 }
 
 // tweet function to display tweet data
 
-function getFeedHtml(){
-    let feedHtml = ``
-    for(let tweet of tweetsData){
+function getFeedHtml() {
 
-        let likeIconClass = ''
-        let retweetIconClass = ''
+  let feedHtml = ``
 
-        if(tweet.isLiked){
-            likeIconClass = 'liked'
-        }
+  tweetsData.forEach((tweet) => {
+    let likeIconClass = ""
+    let retweetIconClass = ""
 
-        if(tweet.isRetweeted){
-            retweetIconClass = 'retweeted'
-        }
+    if (tweet.isLiked) {
+      likeIconClass = "liked"
+    }
 
-        // showing replies from the tweets data
+    if (tweet.isRetweeted) {
+      retweetIconClass = "retweeted"
+    }
 
-        let repliesHtml = ``
+    // showing replies from the tweets data
 
-        if(tweet.replies.length > 0){
-            tweet.replies.forEach((reply)=>{
-                repliesHtml += 
-                `
+    let repliesHtml = ``
+
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach((reply) => {
+        repliesHtml += `
                 <div class="tweet-reply">
                 <div class="tweet-inner">
                 <img src="${reply.profilePic}" class="profile-pic">
@@ -112,13 +137,12 @@ function getFeedHtml(){
                 </div>
                 </div>
                 `
-            })
-        }
+      })
+    }
 
-        // showing tweets data
+    // showing tweets data
 
-    feedHtml +=
-        `
+    feedHtml += `
     <div class="tweet">
       <div class="tweet-inner">
         <img src=${tweet.profilePic} class="profile-pic">
@@ -127,7 +151,7 @@ function getFeedHtml(){
             <p class="tweet-text">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
-                <i class="fa-regular fa-comment-dots " data-reply="${tweet.uuid}"></i>
+                <i class="fa-regular fa-comment-dots" data-reply="${tweet.uuid}"></i>
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
@@ -142,18 +166,23 @@ function getFeedHtml(){
         </div>            
       </div>
    </div>
-   <div class="hidden" id="replies-${tweet.uuid}">
+   <div class="hidden reply-section" id="replies-${tweet.uuid}">
         ${repliesHtml}
+        <div class="tweet-input reply-tweet-input">
+        <img src="images/logo.png" alt="tweet logo" class="logo-img reply-logo-img">
+        <textarea placeholder="Tweet your Reply" class="tweet-area reply-tweet-area" id="reply-tweet-${tweet.uuid}"></textarea>
+        <button class="tweet-btn reply-tweet-btn" id="reply-tweet-btn" data-replytext = "${tweet.uuid}">Reply</button>
+        </div>     
     </div>   
     </div>
     `
-    }
-    return feedHtml
+  })
+  return feedHtml
 }
 
 // render the elements of the tweet on the browser
 
-function render(){
-    document.getElementById('feed').innerHTML = getFeedHtml()
+function render() {
+  document.getElementById("feed").innerHTML = getFeedHtml()
 }
 render()
